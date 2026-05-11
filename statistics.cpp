db976c4292033c1f53ace2d377718b5afea6b2b4
Shirvlad <novttac431@gmail.com>
2026-05-11 16:45:26 +0300

    fix: binomial

---
+++ statistics.cpp
@@ -0,0 +1,17 @@
+include "math/core.hpp"
+
+namespace math {
+	namespace statistics {
+		unsigned long binomial(unsigned long n, unsigned long k, unsigned long m) {
+			if ((k == 0) || (k==n)) {
+				return 1UL;
+			}
+			if (k > n) {
+				return 0UL;
+			}
+
+			return prod(k + 1, n, m) * invmod(factorial(n - k, m), m)) % m;
+		}
+	}
+	
+}
